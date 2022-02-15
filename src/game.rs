ebd465bf67d2c11b4c5d3ce17b47fc93cfab302f
Alexis Bourget <alexis.bourget@gmail.com>
2022-02-15 23:21:28 +0100

    Document game.rs

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,6 @@
 //! Game page ('/game')
+//!
+//! See [`Game`].
 use std::collections::HashSet;
 use std::time::Duration;
 
@@ -16,15 +18,38 @@ use crate::sentences::{Sentence, Sentences};
 use crate::timer::Timer;
 use crate::Route;
 
+/// Minimum time between two sentences.
 const MIN_TIMER_DURATION: Duration = Duration::from_secs(3);
+/// Maximum time between two sentences.
 const MAX_TIMER_DURATION: Duration = Duration::from_secs(60);
+/// String representation for javascript.
 const MIN_TIMER_DURATION_STR: &str = "3";
+/// String representation for javascript.
 const MAX_TIMER_DURATION_STR: &str = "60";
 
+/// Game component.
+///
+/// To be as safe as possible, states are tracked trough an enum; [`State`],
+/// so that only the relevant information can be access in each state.
+///
+/// This is not possible for everything, especially data that must live through
+/// several non-contiguous states.
 pub struct Game {
+    /// Time interval between each sentence.
+    ///
+    /// See [`MIN_TIMER_DURATION`], [`MAX_TIMER_DURATION`] and [`State::Waiting`].
     duration: Duration,
+    /// The sentences selected to play the game.
+    ///
+    /// They are parsed from a [`GameQuery`] on construction.
     sentences: Sentences,
+    /// State of the game.
     state: State,
+    /// Used to access (indirectly) the history to go back to [`/`][Route::Home].
+    ///
+    /// Never read directly but must be present since the handle is
+    /// reference counted and is not tracked by Yew directly to avoid
+    /// keeping unnecessary data when possible.
     _history: HistoryHandle,
 }
 
@@ -38,60 +63,102 @@ impl std::fmt::Debug for Game {
     }
 }
 
+/// States of the game.
 #[derive(Debug)]
-pub enum State { // Don't sort alphabetically here, we want to follow the flow of the game.
+pub enum State {
+    // Don't sort alphabetically here, we want to follow the flow of the game.
+    /// Waiting for permission to play sound.
+    ///
+    /// To avoid noisy ads that autostart and such, browsers asks for at least one user interaction
+    /// with a page before playing sound. Since our whole game is built around sound, we ask for
+    /// that interaction (a click on a button) once at the start.
+    ///
+    /// This would be cumbersome if it was the only thing displayed then so this state also offers
+    /// the possibility of setting the duration between sentences.
     GettingSoundPermission,
+    /// Sound is currently playing.
     Playing {
+        /// The sentence and at which point of if the game is.
         current: (Sentence, SentenceState),
+        /// Reference to the sound node, to play/pause it.
         node: NodeRef,
     },
+    /// Playing is paused.
+    ///
+    /// Since we don't keep the sound node here, it means the current
+    /// sound will restart from the start when 'Resume' is sent.
+    ///
+    /// I did not consider this a default because the sound are very short
+    /// (one to five words) and the game is made for people learning french:
+    /// making them remember half a word and hear the second part several
+    /// seconds later seems like a bad practice.
     PlayingPaused {
+        /// The sentence to resume and which part of it.
         current: (Sentence, SentenceState),
-        // Since we don't keep the sound node here, it means the current
-        // sound will restart from the start when 'Resume' is sent.
-        //
-        // I did not consider this a default because the sound are very short
-        // (one to five words) and the game is made for people learning french:
-        // making them remember half a word and hear the second part several
-        // seconds later seems like a bad practice.
     },
+    /// Waiting for the next sentence.
     Waiting {
         // Both `Interval` and `Timer` are cancelled on drop.
+        /// Sends a message each second to update the countdown
+        /// to the [next sentence][Msg::NextSentence].
         seconds: Interval,
+        /// Countdown display to the next sentence.
         time_left: Duration,
+        /// Coutdown to the next sentence, will send a message
+        /// once complete.
         timer: Timer,
     },
+    /// Waiting for the next sentence is paused.
     WaitingPaused {
+        /// What's left of the countdown to the next sentence.
         time_left: Duration,
     },
+    /// Game is finished.
     Finished,
 }
 
+/// A sentence is composed of two parts (with regard to the sound files).
 #[derive(Debug, Clone, Copy)]
 pub enum SentenceState {
+    /// Second part of the sentence, unique for each element.
     Element,
+    /// First part of the sentence, the same for each element.
     Family,
 }
 
+/// Messages sent during the lifetime of a [`Game`].
 #[derive(Debug)]
 pub enum Msg {
+    /// Update the start duration of the countdown
+    /// to the next sentence.
     ChangeTimer(u64),
+    /// Go back to [`/`][Route::Home].
     GoHome,
+    /// Launch next sentence sound.
     NextSentence,
+    /// Pause the game.
     Pause,
+    /// Resume playing.
     Resume,
-    UpdateTime,
+    /// Either the first or second sound of a whole
+    /// sentence just completed.
     SentenceState,
+    /// Got permission to play sound.
     SoundPermission,
+    /// Update coutdown to next sentence.
+    ///
+    /// See also [`timer_slider()`].
+    UpdateTime,
 }
 
 impl Component for Game {
     type Message = Msg;
-
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
         let link = ctx.link();
+
+        // Unwrap: we are in a `BrowserRouter` switch, see 'crate::switch'.
         let location = link.location().unwrap();
         let families = location
             .query::<GameQuery>()
@@ -100,6 +167,7 @@ impl Component for Game {
             .try_into()
             .unwrap_or_else(|_| HashSet::from_iter(Family::into_enum_iter()));
 
+        // Unwrap: we are in a `BrowserRouter` switch, see 'crate::switch'.
         let history = link
             .add_history_listener(link.callback(|_| Msg::GoHome))
             .unwrap();
@@ -124,7 +192,7 @@ impl Component for Game {
         gloo_console::debug!(format!("{st:?} --- {msg:?}"));
 
         match (&mut self.state, msg) {
-            // State of game: timer duration was changed before game started.
+            // State of game: timer duration was changed before game started or during a pause.
             (State::GettingSoundPermission | State::WaitingPaused { .. } | State::PlayingPaused { .. }, Msg::ChangeTimer(seconds)) => {
                 self.duration = Duration::from_secs(seconds).clamp(
                     MIN_TIMER_DURATION,
@@ -198,7 +266,9 @@ impl Component for Game {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
+
         let state_view = match self.state {
+            // State: we do not yet have permission to play sound.
             State::GettingSoundPermission => html! {
                 <>
                     <button onclick={ ctx.link().callback(|_| Self::Message::SoundPermission) }>
@@ -207,6 +277,7 @@ impl Component for Game {
                     { timer_slider(link, self.duration) }
                 </>
             },
+            // State: sound is currently playing.
             State::Playing {
                 current, ref node, ..
             } => html! {
@@ -215,19 +286,23 @@ impl Component for Game {
                     { pause_button(link) }
                 </>
             },
+            // State: sound was paused.
             State::PlayingPaused { .. } => html! { resume_view(link, self.duration) },
+            // State: waiting for the coutdown to the next sentence to end.
             State::Waiting { time_left, .. } => html! {
                 <>
                     <p> { format!("Phrase suivante dans ... {}s", time_left.as_secs()) } </p>
                     { pause_button(link) }
                 </>
             },
+            // State: countdown to next sentence was paused.
             State::WaitingPaused { time_left, .. } => html! {
                 <>
                     <p> { format!("Phrase suivante dans ... {}s (Pause)", time_left.as_secs()) } </p>
                     { resume_view(link, self.duration) }
                 </>
             },
+            // State: game is finished, nothing more to do.
             State::Finished => html! {
                 <>
                     <p> { "Jeu terminé !" } </p>
@@ -245,6 +320,12 @@ impl Component for Game {
     }
 }
 
+/// Produce a [`State::Waiting`] instance filled correctly with the
+/// time left for the [`Timer`] to the next sentence and sending the
+/// [`Msg::UpdateTime`] every second for the countdown display.
+///
+/// Used on [`Msg::Resume`] and when the [`SentenceState::Element`] sound
+/// finishes and the countdown to the next sentence must be launched.
 fn waiting_state(link: &Scope<Game>, time_left: Duration) -> State {
     State::Waiting {
         time_left,
@@ -261,6 +342,8 @@ fn waiting_state(link: &Scope<Game>, time_left: Duration) -> State {
         },
     }
 }
+
+/// Invisible audio player which contains the audio HTML element to play the sentences.
 fn audio_player(
     link: &Scope<Game>,
     (sentence, sentence_state): (Sentence, SentenceState),
@@ -268,7 +351,7 @@ fn audio_player(
 ) -> Html {
     let src = match sentence_state {
         SentenceState::Family => sentence.family_sound_file(),
-        SentenceState::Element => sentence.sentence_sound_file(),
+        SentenceState::Element => sentence.element_sound_file(),
     };
     let onended = link.callback(|_| Msg::SentenceState);
 
@@ -287,6 +370,7 @@ fn audio_player(
     }
 }
 
+/// Slider to select the duration of the next countdown to the next sentence.
 fn timer_slider(link: &Scope<Game>, current_duration: Duration) -> Html {
     html! {
         <>
@@ -299,6 +383,8 @@ fn timer_slider(link: &Scope<Game>, current_duration: Duration) -> Html {
                 value={ format!("{}", current_duration.as_secs()) }
                 oninput={
                     link.callback(|e: InputEvent| {
+                        // Unchecked: we define the callback inside the element it concerns, we cannot
+                        // be referencing the wrong one.
                         let input: HtmlInputElement = e.target_unchecked_into();
                         Msg::ChangeTimer(input.value_as_number().round().clamp(0.0, u64::MAX as _) as u64)
                     })
@@ -309,9 +395,16 @@ fn timer_slider(link: &Scope<Game>, current_duration: Duration) -> Html {
     }
 }
 
+/// Button to click on to [pause][Msg::Pause] the game.
 fn pause_button(link: &Scope<Game>) -> Html {
     html! { <button onclick={ link.callback(|_| Msg::Pause) }> { "Pause" } </button> }
 }
+
+/// View shown when the game is paused.
+///
+/// It displays a ["Reprendre"][Msg::Resume] button, a [slider][timer_slider()]
+/// to select the duration of the next coutdown to the next sentence and a
+/// [button to go home][go_home_button()].
 fn resume_view(link: &Scope<Game>, current_duration: Duration) -> Html {
     html! {
         <>
@@ -322,15 +415,27 @@ fn resume_view(link: &Scope<Game>, current_duration: Duration) -> Html {
     }
 }
 
+/// Button to go back to ['/'][crate::app::App] and selecting families.
+///
+/// This **needs** an [`HistoryHandle`] to be present in the [`Game`] struct
+/// else it will panic trying to access it.
 fn go_home_button(link: &Scope<Game>) -> Html {
+    // Unwrap: there is an `HistoryHandle` saved in the `Game` struct,
+    // whatever the state is.
     let history = link.history().unwrap();
+    // Once: since we change page, this button will disappear and cannot be clicked twice.
     let onclick = Callback::once(move |_| history.push(Route::Home));
 
     html! { <button {onclick}> { "Retourner à la sélection de familles" } </button> }
 }
 
+/// When going to the '/game' page, a query can be provided to select only some families.
+/// If no query is present or it is invalid, all families are selected as a default.
 #[derive(Clone, PartialEq, Serialize, Deserialize)]
 pub struct GameQuery {
+    /// The families to select, as a bitmask.
+    ///
+    /// See [`Family`] for more details.
     families: u8,
 }
 
